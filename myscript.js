64e64822af91e97cd0bddd1e51b1bb1ecb4059a2
abey-noble <68761349+abey-noble@users.noreply.github.com>
2021-04-11 23:51:22 -0400

    Add files via upload
    tablet view

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+
+
+
+var menu_icon = document.querySelector("material-icons-outlined-menu");
+var menu = document.querySelector("menu_web");
+
+
+menu_icon.addEventListener("click", function() {
+    menu_icon.style.display = "none";
+    menu.style.display = "";
+})
+
+
